65d11c022d76398ff80a3a0f08cb5e248a508961
MarcosDiaz-10 <diazelhalabimarcosa@gmail.com>
2024-06-21 18:04:14 -0400

    proyecto actualizado con listas, faltan los patrones

--- proyectoAYED.cpp
+++ proyectoAYED.cpp
@@ -1,160 +1,161 @@
 #include<iostream>
 using namespace std;
 
-class letra {
-    int patron;
-    char paquete[7][5];
+template <typename T> class Node {
+    public:
+    T payload;
+    Node <T>*prev;
+    Node <T>*next;
+
+    Node () {
+        prev=next=nullptr;
+    }
+
+};
+
+
+template <typename T> class List {
+    public:
+    Node<T> *first;
+    Node<T> *last;
+
+    List() {
+        this->first=this->last=nullptr;
+    }
+
+    void next(Node<T> &*n) {
+        if(n!= nullptr){
+            n=n->next;
+        }
+    }
+    void prev(Node<T> &*n) {
+        if(n!= nullptr){
+            n=n->prev;
+        }
+    }
+
+
+    void add( T x) {
+        Node<T> *p = new Node();
+        p-> payload = x;
+        if(this->first == nullptr ){
+            this->first = p;
+            this->last = p;
+            return;
+        }
+        this->last.next = p;
+        p->prev=last;
+        this->last =p;
+    }
+    
+};
+
+
+void devolverSegmentos( string p, int fila, string &seg1, string &seg2, string &seg3, string &seg4, string patron ) {
+    if( fila < 4 ) {
+            for (int i = 0; i < 5; i++)
+            {
+                if( i < 3 ) {
+                    seg1 = seg1 + p[i];
+                }
+                if( i > 3 ) {
+                    seg2 = seg2 + p[i];
+                }
+            }
+            
+        }    
+        if( fila > 4 ) {
+            for (int i = 0; i < 5; i++)
+            {
+                if( i < 3 ) {
+                    seg3 = seg3 + p[i];
+                }
+                if( i > 3 ) {
+                    seg4 = seg4 + p[i];
+                }
+            }
+            
+        }    
+}
+
+class Segmento {
+    string segmento;
+    bool estaUsado;
+
+    Segmento(string entrada) {
+        segmento = entrada;
+    }
+};
+
+class Letra {
+    string patron;
+    string segmento1;
+    string segmento2;
+    string segmento3;
+    string segmento4;
+
 
     public:
-    letra() {
-        patron = 0;
+    Letra() {
+        patron = "0";
         for( int i = 0; i < 7; i++) {
-            for( int j=0; j < 5; j++) {
-                paquete[i][j] = ' ';
-            }
+            segmento1[i] = 0;
+            segmento2[i] = 0;
+            segmento3[i] = 0;
+            segmento4[i] = 0;
         }
     }
     
-    void setPatron(int p) {
+    void setPatron(string p) {
         patron = p;
     }
 
-    void setPaquete(string p, int fila) {
-        for( int i = 0; i < 5; i++) {
-            paquete[fila][i] = p[i];
-        }
+    void setPaquete(string seg1, string seg2, string seg3, string seg4) {
+        segmento1 = seg1;
+        segmento2 = seg2;
+        segmento3 = seg3;
+        segmento4 = seg4;
     }
 
-    void getPaquetes() {
-        for( int j = 0; j < 7; j++) {
-            for( int t = 0; t < 5; t++){
-                cout << paquete[j][t];
-            }
-            cout<< "paquete " << j << endl;
-        }
-    }
-    
-    int getPatron () {
-        return patron;
-    }
 
 };
 
-class Patron {
-   public:
-   char patrones[7];
-   
-   Patron(string p){
-        for (int i = 0; i < 7; i++)
-        {
-            patrones[i]=p[i];
-        }
-        
-   };
-
-   
-
-};
-
-Patron p = Patron("0111101");
-
-Patron arrPatrones[27] = {
-Patron("1001001"),Patron("1000001"),Patron("0001000"),Patron("0000001"),Patron("0010101"),Patron("1111111"),Patron("0010100"),Patron("1000101"),Patron("1001000"),Patron("0100100"),Patron("0000100"),Patron("0000111"),Patron("0001001"),Patron("0110110"),Patron("0010000"),Patron("1010001"),Patron("0001110"),Patron("0010001"),Patron("1001111"),Patron("1000100"),Patron("0001100"),Patron("1001100"),Patron("1111110"),Patron("0011000"),Patron("0111111"),Patron("0000110"),Patron("0010100")
-};
-
- bool isPatron (char arrActual[], Patron arrPatrones[], letra paqueteActual) {
-    string patronActual="", patronComparar="";
-    bool isPatron= false;
-
-    for(int i=0;i<27;i++){
-        for (int j = 0; j < 7; j++)
-        {
-            patronActual += arrPatrones[i].patrones[j];
-            patronComparar += arrActual[j];
-            
-            if( j == 6 ) {
-                if( patronActual == patronComparar ) {
-                    paqueteActual.setPatron(j);
-                    isPatron=true;
-                    return isPatron;
-                }        
-            }
-        }
-         patronActual = "";
-         patronComparar = "";
-
-    }
-    return isPatron;
-}
-
-
-
-void entrada( letra *(&paquetes)){
-    int numPaquetes = 0;
-    paquetes = new letra[numPaquetes];
-    letra *auxPaquetes = new letra[numPaquetes];
-    char *patronActual = new char[7];
+List<Letra> entrada () {
+    List<Letra> *paquetes = new List<Letra>();
     string digito;
+    string seg1;
+    string seg2;
+    string seg3;
+    string seg4;
+    string patron;
 
     int j=1;
-    letra paqueteActual;
+    Letra paqueteActual;
+
+
     do
     {
         cin >> digito;
-        if( digito == "ENJOY") return;
+        if(digito == "ENJOY") break;
+        
+        if( j<7) {
+            devolverSegmentos(digito, j-1, seg1, seg2, seg3, seg4,patron);
 
-        if( j < 7 ) {
-
-            paqueteActual.setPaquete(digito,j - 1);
-            patronActual[j - 1] = digito[2];
-            j++;
         } else {
+            devolverSegmentos(digito, j-1, seg1, seg2, seg3, seg4,patron);
 
-            patronActual[j - 1] = digito[2];
-            paqueteActual.setPaquete(digito,j - 1);
-
-            bool ifPatron = isPatron(patronActual,arrPatrones,paqueteActual);
-
-            if( !ifPatron ) continue;
-
-            delete []auxPaquetes;
-            numPaquetes=numPaquetes+1;
-            auxPaquetes = new letra[numPaquetes];
-            for( int i = 0; i < numPaquetes-1; i++) {// cuando se aumenta numpaquetes
-                auxPaquetes[i] = paquetes[i];
-            }
-
-            auxPaquetes[numPaquetes - 1] = paqueteActual; 
+            paquetes->add(paqueteActual);
             
-            delete []paquetes;
-            paquetes = new letra[numPaquetes];
-            for( int i = 0; i < numPaquetes; i++) {
-                paquetes[i] = auxPaquetes[i];
-            }
-            paqueteActual = letra();
-            j = 1;
-            continue;
-
         }
 
-        
-     
-    } while (digito != "ENJOY");
+    } while ( digito == "ENJOY");
     
 
-
+    return *paquetes;
 }
 
+int main () {
 
 
-
-int main(){
-
-    letra *paquetes;
-    entrada(paquetes);
-
-    paquetes[1].getPaquetes();
-    paquetes[1].getPatron();
-
-    delete []paquetes;
+    return 0;
 }
